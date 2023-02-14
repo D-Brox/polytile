4b04a20da13b32a634e60aaf5d95117ce0eb86eb
David Brochero <davidfbg00@gmail.com>
2023-02-14 18:06:51 -0300

    Fix logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
+use indicatif::ParallelProgressIterator;
 use rayon::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
+
 mod tiles;
-use tiles::{bit_masked_tiles,number2matrix};
+use tiles::{bit_masked_tiles, number2matrix};
 
 fn u64_2str_matrix<'a>(matrix: &[Vec<u64>], mapping: &'a [&'a str]) -> Vec<Vec<&'a str>> {
     let mut str_matrix = vec![];
@@ -17,29 +19,37 @@ fn u64_2str_matrix<'a>(matrix: &[Vec<u64>], mapping: &'a [&'a str]) -> Vec<Vec<&
     str_matrix
 }
 
-fn vec2array<T, const N: usize>(v: Vec<T>) -> [T; N] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
-}
-
 fn main() -> Result<()> {
     let tilefile = BufReader::new(File::open("tiles.txt")?);
-    let tiles = bit_masked_tiles(tilefile);
+    let all_tiles = bit_masked_tiles(tilefile);
+    // let mask0 = (1<<11)+ (1<<8)+ (1<<45);
     let mask0 = 0;
+    let tiles: Vec<u64> = all_tiles
+        .iter()
+        .filter(|t| (**t & mask0) == 0)
+        .cloned()
+        .collect();
 
+    let first_tiles: Vec<u64> = tiles
+        .iter()
+        .take_while(|&&m| m < (1 << 52))
+        .cloned()
+        .collect();
     // Find valid solutions using the bit masks.
-    let mask_solutions: Vec<[u64; 10]> = tiles
-        .par_iter() // Now we just do Knuth's Algorithm X
+    let mask_solutions: Vec<Vec<u64>> = first_tiles
+        .par_iter()
+        .progress_count(first_tiles.len() as u64)
         .flat_map(|&m1| {
+            // Now we just do Knuth's Algorithm X
             let mask1 = m1 | mask0;
-            let filter_loop = |mask: u64, m: u64, uniq: &[u64], length: u64| {
+            let filter_loop = |mask: u64, m: u64, uniq: &[u64], length: u64| -> Vec<Vec<u64>> {
                 fn filter(mask: u64, m: u64, uniq: &[u64], masked: &mut Vec<u64>) {
                     masked.clear();
                     masked.extend(
-                        uniq.iter()
-                            .copied()
-                            .take_while(|m1| *m1 < m) // No permutations.
-                            .filter(|m1| m1 & mask == 0), // No intersection.
+                        uniq.iter().copied().filter(|m1| {
+                            let m2 = m.next_power_of_two();
+                            (m1 & mask == 0) && (*m1 >= m2) && (*m1 < m2 << 1)
+                        }), // No intersection. Next tile type
                     );
                 }
                 fn filter_loop(
@@ -54,12 +64,10 @@ fn main() -> Result<()> {
                     if depth == 0 {
                         sol.push(curr.to_vec());
                     } else if depth == 1 {
-                        for m1 in uniq.iter().take_while(|&m1| *m1 > m) {
-                            if m1 & mask == 0 {
-                                curr.push(*m1);
-                                filter_loop(0, curr, sol, 0, 0, &[0], 0);
-                                curr.pop();
-                            }
+                        for &m1 in uniq.iter().filter(|&m1| m1 & mask == 0) {
+                            curr.push(m1);
+                            sol.push(curr.to_vec());
+                            curr.pop();
                         }
                     } else {
                         let mut masked = Vec::with_capacity(total);
@@ -77,14 +85,7 @@ fn main() -> Result<()> {
                 s
             };
 
-            let s = filter_loop(mask1, m1, &tiles, 10);
-            let mut solutions = Vec::new();
-
-            for i in s {
-                let j: [u64; 10] = vec2array(i);
-                solutions.push(j)
-            }
-            solutions
+            filter_loop(mask1, m1, &tiles, 10)
         })
         .collect();
 
@@ -93,7 +94,7 @@ fn main() -> Result<()> {
         for s in solution {
             let mask = s & ((1u64 << 50) - 1);
             let kind = s >> 50;
-            let mat = number2matrix(10, 5, mask);
+            let mat = number2matrix(5, 10, mask);
             for (i, row) in mat.iter().enumerate() {
                 for (j, &value) in row.iter().enumerate() {
                     if value {
@@ -101,14 +102,12 @@ fn main() -> Result<()> {
                     }
                 }
             }
-            println!(" ");
         }
-    }
-
-    let mapping = &[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-    let str_matrix = u64_2str_matrix(&num_grid, mapping);
-    for line in str_matrix {
-        println!("{:?}", line)
+        let mapping = &[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+        let str_matrix = u64_2str_matrix(&num_grid, mapping);
+        for line in str_matrix {
+            println!("{:?}", line)
+        }
     }
 
     Ok(())
--- src/tiles.rs
+++ src/tiles.rs
@@ -3,11 +3,11 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn rotate(matrix: &[Vec<bool>]) -> Vec<Vec<bool>> {
-    let m = matrix.len();
-    let n = matrix[0].len();
+    let m = matrix[0].len();
+    let n = matrix.len();
     let mut rotated = vec![vec![false; n]; m];
     for (i, j) in (0..n).cartesian_product(0..m) {
-        rotated[j][n - i - 1] = matrix[i][j];
+        rotated[j][(n - i - 1) % n] = matrix[i][j];
     }
     rotated
 }
@@ -31,16 +31,6 @@ fn rotations_and_mirrors(matrix: &[Vec<bool>]) -> Vec<Vec<Vec<bool>>> {
     rotations
 }
 
-fn matrix2number(matrix: &[Vec<bool>]) -> u16 {
-    let mut number = 0u16;
-
-    for (i, j) in (0..4).cartesian_product(0..4) {
-        number |= (matrix[i][j] as u16) << (4 * i + j);
-    }
-    number
-}
-
-
 fn submatrix2matrices(matrix: &Vec<Vec<bool>>, a: usize, b: usize) -> Vec<Vec<Vec<bool>>> {
     let m = matrix.len();
     let n = matrix[0].len();
@@ -54,8 +44,20 @@ fn submatrix2matrices(matrix: &Vec<Vec<bool>>, a: usize, b: usize) -> Vec<Vec<Ve
         }
         result.push(temp);
     }
-
     result
+}
+
+fn matrix2number(matrix: &[Vec<bool>]) -> u64 {
+    let mut number = 0u64;
+    let m = matrix.len();
+    let n = matrix[0].len();
+
+    for (i, j) in (0..m).cartesian_product(0..n) {
+        number = number << 1;
+        number |= matrix[i][j] as u64;
+    }
+    // println!("{:0width$b}",number,width=64);
+    number
 }
 
 pub fn number2matrix(m: usize, n: usize, number: u64) -> Vec<Vec<bool>> {
@@ -80,14 +82,14 @@ pub fn bit_masked_tiles(tilefile: BufReader<File>) -> Vec<u64> {
             let n = parts.next().unwrap().parse::<usize>().unwrap();
             let b = u64::from_str_radix(parts.next().unwrap(), 2).unwrap();
             let matrix = number2matrix(m, n, b);
-            let matrices = submatrix2matrices(&matrix, 10, 5);
+            let matrices = rotations_and_mirrors(&matrix);
             matrices
                 .iter()
-                .flat_map(|matrix| rotations_and_mirrors(matrix))
-                .map(|matrix| (matrix2number(&matrix) as u64) | 1 << (50 + l))
+                .unique()
+                .flat_map(|matrix| submatrix2matrices(matrix, 5, 10))
+                .map(|matrix| (matrix2number(&matrix) as u64) + (1u64 << (50 + l)))
                 .collect()
         })
         .sorted()
-        .dedup()
         .collect_vec()
 }
